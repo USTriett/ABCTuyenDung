fa08542877137179f81db7dafd775b65cca240bf
TrietHuynh443 <triethuynh4403@gmail.com>
2024-07-02 15:49:55 +0700

    add dangkydangtuyenGUI

--- ABCTuyenDung/Form1.Designer.cs
+++ ABCTuyenDung/Form1.Designer.cs
@@ -29,12 +29,257 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.mainPanel = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.formLabel = new System.Windows.Forms.Label();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.bindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.submitBtn = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
+            this.dateTimePickerLayout.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(725, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Location = new System.Drawing.Point(79, 241);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(724, 235);
+            this.textBox2.TabIndex = 1;
+            // 
+            // mainPanel
+            // 
+            this.mainPanel.AutoScroll = true;
+            this.mainPanel.AutoScrollMargin = new System.Drawing.Size(0, 50);
+            this.mainPanel.Controls.Add(this.submitBtn);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Controls.Add(this.dateTimePickerLayout);
+            this.mainPanel.Controls.Add(this.label5);
+            this.mainPanel.Controls.Add(this.textBox4);
+            this.mainPanel.Controls.Add(this.label4);
+            this.mainPanel.Controls.Add(this.textBox3);
+            this.mainPanel.Controls.Add(this.label3);
+            this.mainPanel.Controls.Add(this.label2);
+            this.mainPanel.Controls.Add(this.button1);
+            this.mainPanel.Controls.Add(this.formLabel);
+            this.mainPanel.Controls.Add(this.textBox2);
+            this.mainPanel.Controls.Add(this.textBox1);
+            this.mainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanel.Location = new System.Drawing.Point(0, 0);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
+            this.mainPanel.Name = "mainPanel";
+            this.mainPanel.Padding = new System.Windows.Forms.Padding(20, 20, 0, 50);
+            this.mainPanel.Size = new System.Drawing.Size(880, 545);
+            this.mainPanel.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(77, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Thời gian tuyển dụng";
+            // 
+            // dateTimePickerLayout
+            // 
+            this.dateTimePickerLayout.Controls.Add(this.panel1);
+            this.dateTimePickerLayout.Controls.Add(this.panel3);
+            this.dateTimePickerLayout.Controls.Add(this.panel4);
+            this.dateTimePickerLayout.Location = new System.Drawing.Point(78, 166);
+            this.dateTimePickerLayout.Margin = new System.Windows.Forms.Padding(3, 3, 3, 0);
+            this.dateTimePickerLayout.Name = "dateTimePickerLayout";
+            this.dateTimePickerLayout.Size = new System.Drawing.Size(725, 56);
+            this.dateTimePickerLayout.TabIndex = 12;
+            this.dateTimePickerLayout.Paint += new System.Windows.Forms.PaintEventHandler(this.dateTimePickerLayout_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.panel1.Size = new System.Drawing.Size(270, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(279, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(160, 53);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(445, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.panel4.Size = new System.Drawing.Size(270, 50);
+            this.panel4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 556);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Yêu cầu ứng viên";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(79, 575);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(724, 235);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Số lượng tuyển dụng";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 522);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(724, 22);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mô tả vị trí tuyển dụng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Vị trí tuyển dụng";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Location = new System.Drawing.Point(677, 46);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Trang chủ";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // formLabel
+            // 
+            this.formLabel.AutoSize = true;
+            this.formLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.formLabel.Location = new System.Drawing.Point(32, 46);
+            this.formLabel.Name = "formLabel";
+            this.formLabel.Size = new System.Drawing.Size(280, 36);
+            this.formLabel.TabIndex = 2;
+            this.formLabel.Text = "Đăng ký đăng tuyển";
+            this.formLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // submitBtn
+            // 
+            this.submitBtn.Location = new System.Drawing.Point(357, 843);
+            this.submitBtn.Name = "submitBtn";
+            this.submitBtn.Size = new System.Drawing.Size(190, 43);
+            this.submitBtn.TabIndex = 0;
+            this.submitBtn.Text = "Hoàn tất";
+            this.submitBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(880, 545);
+            this.Controls.Add(this.mainPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Đăng ký đăng tuyển";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
+            this.dateTimePickerLayout.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel mainPanel;
+        private System.Windows.Forms.Label formLabel;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.BindingSource bindingSource2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel dateTimePickerLayout;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button submitBtn;
     }
 }
 
--- ABCTuyenDung/Form1.cs
+++ ABCTuyenDung/Form1.cs
@@ -12,12 +12,30 @@ namespace ABCTuyenDung
 {
     public partial class Form1 : Form
     {
+        private IDatePicker _startDate;
+        private IDatePicker _endDate;
         public Form1()
         {
+            _startDate = new IDatePicker();
+            _endDate = new IDatePicker();
+
             InitializeComponent();
+
+
         }
         public void TestSqlConnector(string connector)
         {
+
+        }
+
+        private void dateTimePickerLayout_Paint(object sender, PaintEventArgs e)
+        {
+            FlowLayoutPanel flowLayoutPanel = (FlowLayoutPanel)sender;
+
+            Panel panel1 = flowLayoutPanel.Controls[0] as Panel;
+            panel1.Controls.Add(_startDate);
+            panel1 = flowLayoutPanel.Controls[2] as Panel;
+            panel1.Controls.Add(_endDate);
 
         }
     }
